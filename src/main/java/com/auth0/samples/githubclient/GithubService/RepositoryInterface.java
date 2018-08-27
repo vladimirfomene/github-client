adb96c0d6a5c7c6eb80a96416f3c97551118c801
Vladimir Fomene <vladimirfomene@gmail.com>
2018-08-27 18:22:36 +0100

    Add method for creating repos to interface

--- src/main/java/com/auth0/samples/githubclient/GithubService/RepositoryInterface.java
+++ src/main/java/com/auth0/samples/githubclient/GithubService/RepositoryInterface.java
@@ -7,10 +7,15 @@ import retrofit2.http.GET;
 
 import com.auth0.samples.githubclient.models.GithubRepository;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface RepositoryInterface {
 
     @GET("user/repos")
-    Call<List<GithubRepository>> listRepos(@Header("Authorization") String accessToken);
+    Call<List<GithubRepository>> listRepos(@Header("Authorization") String accessToken, @Header("Accept") String apiVersionSpec);
+
+    @POST("user/repos")
+    Call<GithubRepository> createRepo(@Header("Authorization") String accessToken, @Header("Accept") String apiVersionSpec,
+                                      @Header("Content-Type") String contentType);
 }
